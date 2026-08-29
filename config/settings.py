c9a0173ae3da40dacd3053927c3495a4048c5062
Jose Quintero <aresjq21@gmail.com>
2026-08-29 10:10:58 -0500

    Cambio a SendGrid para correos

--- config/settings.py
+++ config/settings.py
@@ -227,37 +227,16 @@ EPAYCO_LANG = os.getenv(
 
 
 # ============================================================
-# CORREO
+# CORREO (SendGrid)
 # ============================================================
 
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST = os.getenv(
-    'EMAIL_HOST',
-    'smtp.gmail.com'
-)
-
-EMAIL_PORT = int(
-    os.getenv(
-        'EMAIL_PORT',
-        '587'
-    )
-)
-
-EMAIL_USE_TLS = os.getenv(
-    'EMAIL_USE_TLS',
-    'True'
-).lower() == 'true'
-
-EMAIL_HOST_USER = os.getenv(
-    'EMAIL_HOST_USER'
-)
-
-EMAIL_HOST_PASSWORD = os.getenv(
-    'EMAIL_HOST_PASSWORD'
-)
-
-DEFAULT_FROM_EMAIL = os.getenv(
-    'DEFAULT_FROM_EMAIL',
-    EMAIL_HOST_USER
-)
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'aresjq21@gmail.com')
